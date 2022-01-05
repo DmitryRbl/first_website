57cf78f4de5e0756d07c1e2203d3c190e8d81471
dmitryRnb <diman2012.titov@yandex.ru>
2022-01-05 21:09:41 +0300

    feat: попытка добавить плавные переходы
    не смог запретить событие клика до окончания приветствия

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let start = true;
+let counter = 0;
 
-  
-  let counter = 0;
-  
-  let button = document.querySelector('.button');
-  let count = document.querySelector('.count');
-  
-  button.addEventListener('click', function () {
+let button = document.querySelector('.button');
+let count = document.querySelector('.count');
+let cardTitle = document.querySelector('.card-title');
+
+button.addEventListener('click', function () {
+if (start) {
     counter+=1;
     count.textContent = "Ты нажал: " + counter + " раз)";
-  }); 
+}
+}); 
+
+smoothly(cardTitle, 'textContent', 'Привет');
+smoothly(cardTitle, 'textContent', 'Если хочешь увидеть что я для тебя подготовил');
+smoothly(cardTitle, 'textContent', 'Нажми на меня!');
+
+// window.addEventListener('transitionend',function(e){ //Smooth dispatches the 'sm_all_rendered' event when every item has been rendered
+//     start = !start;
+//     console.log(e);
+//   });
+
+
+
+ 
